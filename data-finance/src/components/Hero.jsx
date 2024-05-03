d7b0ae3a9af76563b5763e3c3b6fcb07e721c876
SardiusJay <abidoyejay@gmail.com>
2024-05-04 00:33:23 +0100

    wip:working on the hero section

--- data-finance/src/components/Hero.jsx
+++ data-finance/src/components/Hero.jsx
@@ -4,6 +4,8 @@ const Hero = () => {
   return (
     <div className='text-white'>
     <div className='max-w-[800px] mt-[-96px] w-fall h-screen mx-auto text-center flex flex-col justify-center'>
+    <p>GROWING WITH DATA ANALYTICS</p>
+    <h1>Grow with data</h1>
 
     
     </div>
