cea8d15a635cfc7e18e0b7c2c613f87b85ca07f9
SardiusJay <abidoyejay@gmail.com>
2024-05-04 00:17:00 +0100

    wip:added transition to the navbar

--- data-finance/src/components/Navbar.jsx
+++ data-finance/src/components/Navbar.jsx
@@ -11,14 +11,14 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
     <h1 className='w-full text-3xl font-bold text-[#00df9a]'>QUICKGET.</h1>
-    <ul className='flex hidden'>
+    <ul className='hidden md:flex'>
     <li className='p-4'>Home</li>
     <li className='p-4'>Company</li>
     <li className='p-4'>Resources</li>
     <li className='p-4'>About</li>
     <li className='p-4'>Contact</li>
     </ul>
-    <div onClick={handleNav}>
+    <div onClick={handleNav} className='block md:hidden'>
 
     {!nav ? <AiOutlineClose size={20} /> : <AiOutlineClose size={20} />}
      <AiOutlineMenu size={20} />
