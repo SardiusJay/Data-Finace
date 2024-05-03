89fc744579bfa144a080256854663254b48d4634
SardiusJay <abidoyejay@gmail.com>
2024-05-04 00:24:49 +0100

    wip:added hero section

---
+++ data-finance/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='text-white'>
+    <div className='max-w-[800px] mt-[-96px] w-fall h-screen mx-auto text-center flex flex-col justify-center'>
+
+    
+    </div>
+    
+    </div>
+  )
+}
+
+export default Hero;
