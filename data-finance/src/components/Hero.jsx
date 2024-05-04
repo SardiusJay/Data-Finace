9a6ff3043097a8aa4292e1025b6794d6e4b12822
SardiusJay <abidoyejay@gmail.com>
2024-05-04 01:05:12 +0100

    wip:installing react-typed

--- data-finance/src/components/Hero.jsx
+++ data-finance/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ReactTyped } from "react-typed";
 
 const Hero = () => {
   return (
@@ -6,8 +7,19 @@ const Hero = () => {
     <div className='max-w-[800px] mt-[-96px] w-fall h-screen mx-auto text-center flex flex-col justify-center'>
     <p className=' text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
     <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data</h1>
-    <div>
+
+    <div className='flex justify-center items-center'>
     <p className='md:text-5xl sm:text-4xl text-xl font-bold'>Fast, flexible finanacing for </p>
+    <ReactTyped className='md:text-5xl sm:text-4xl text-xl font-bold'
+    strings={[
+        "BTB",
+        "BTC",
+        "SASS"
+      ]}
+      typeSpeed={120}
+      backSpeed={140}
+      loop
+      />
     </div>
 
     
