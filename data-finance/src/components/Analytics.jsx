48af09d33d29b60a3dad7566cf0a8cd727cfb860
SardiusJay <abidoyejay@gmail.com>
2024-05-04 06:34:34 +0100

    wip:working on the analytics section

--- data-finance/src/components/Analytics.jsx
+++ data-finance/src/components/Analytics.jsx
@@ -5,12 +5,13 @@ const Analytics = () => {
   return (
     <div className='w-full bg-white py-16 px-4 '>
         <div className='max-w-[1240px] mx-auto grid md:grid-cols-2 '>
-            <img src={Laptop} alt="/" />
-            <div className=''>
+            <img className='w-[500] mx-auto my-4' src={Laptop} alt="/" />
+            <div className='flex flex-col justify-center'>
                 <p className='font-bold text-[#00df9a]'>DATA ANALYTICS DASHBOARD</p>
-                <h1 className='font-bold '>Manage Data Analytics Centrally</h1>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2 '>Manage Data Analytics Centrally</h1>
                 <p>orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, 
                 and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3'>Get Started</button>
 
             </div>
         
