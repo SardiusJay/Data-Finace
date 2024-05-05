9188e3583c6032bc2977d137ea0e0ad5148324e1
SardiusJay <abidoyejay@gmail.com>
2024-05-05 23:58:26 +0100

    wip:working on the cards section

--- data-finance/src/components/Cards.jsx
+++ data-finance/src/components/Cards.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import single from '../assets/single.png';
+
 
 const Cards = () => {
   return (
-    <div className='w-full py-110reml px-4  bg-white'>
+    <div className='w-full py-110reml px-4 bg-white'>
     <div className='max-w-[1240px] mx-auto' grid md:grid-cols-3 gap-8>
+        <div className='w-full'>
+            <img className='w-20 mx-auto mt-[-3rem] bg-white' src={single} alt="/" />
+            <h2>Single User</h2>
+            <p>$149</p>
+            <div className=''>
+                <p>500 GB Storage</p>
+                <p>1 Granted User</p>
+                <p>Send up to 2 GB</p>
+            </div>
+            <button>Start Trial</button>
+        </div>
     
     
     </div>
