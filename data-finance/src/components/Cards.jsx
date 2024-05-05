9f39df7eaf9ce457ad3674021b2401a84d786490
SardiusJay <abidoyejay@gmail.com>
2024-05-06 00:18:24 +0100

    wip:working on the cards section

--- data-finance/src/components/Cards.jsx
+++ data-finance/src/components/Cards.jsx
@@ -6,9 +6,9 @@ const Cards = () => {
   return (
     <div className='w-full py-110reml px-4 bg-white'>
     <div className='max-w-[1240px] mx-auto' grid md:grid-cols-3 gap-8>
-        <div className='w-full'>
+        <div className='w-full border'>
             <img className='w-20 mx-auto mt-[-3rem] bg-white' src={single} alt="/" />
-            <h2>Single User</h2>
+            <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
             <p>$149</p>
             <div className=''>
                 <p>500 GB Storage</p>
