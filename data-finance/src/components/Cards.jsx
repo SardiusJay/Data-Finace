34342f40f65335d42835d86ae77dcbb206a3a3cb
SardiusJay <abidoyejay@gmail.com>
2024-05-06 00:26:35 +0100

    wip:working on the cards section

--- data-finance/src/components/Cards.jsx
+++ data-finance/src/components/Cards.jsx
@@ -6,7 +6,7 @@ const Cards = () => {
   return (
     <div className='w-full py-110reml px-4 bg-white'>
     <div className='max-w-[1240px] mx-auto' grid md:grid-cols-3 gap-8>
-        <div className='w-full border'>
+        <div className='w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale(105)'>
             <img className='w-20 mx-auto mt-[-3rem] bg-white' src={single} alt="/" />
             <h2 className='text-2xl font-bold text-center py-8'>Single User</h2>
             <p>$149</p>
