ea5f3075515ce6ebaa6866ec8c405355933fecfb
SardiusJay <abidoyejay@gmail.com>
2024-05-06 11:39:00 +0100

    wip:working on the cards section

--- data-finance/src/components/Cards.jsx
+++ data-finance/src/components/Cards.jsx
@@ -12,20 +12,20 @@ const Cards = () => {
             <p className='text-center text-4xl font-bold '>$149</p>
             <div className='text-center font-medium '>
                 <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-                <p className='py-2 border-b mx-8>1 Granted User</p>
-                <p className='py-2 border-b mx-8>Send up to 2 GB</p>
+                <p className='py-2 border-b mx-8'>1 Granted User</p>
+                <p className='py-2 border-b mx-8'>Send up to 2 GB</p>
             </div>
             <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Start Trial</button>
         </div>
 
-        <div className='w-full shadow-xl flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300  '>
+        <div className='w-full shadow-xl bg-gray-100 flex flex-col p-4 md:my-0 my-8 rounded-lg hover:scale-105 duration-300  '>
         <img className='w-20 mx-auto mt-[-3rem] bg-white' src={single} alt="/" />
         <h2 className='text-2xl font-bold text-center py-8'>Partnership</h2>
         <p className='text-center text-4xl font-bold '>$199</p>
         <div className='text-center font-medium '>
-            <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-            <p className='py-2 border-b mx-8>1 Granted User</p>
-            <p className='py-2 border-b mx-8>Send up to 2 GB</p>
+            <p className='py-2 border-b mx-8 mt-8'>1 TB Storage</p>
+            <p className='py-2 border-b mx-8>3 Allowed User</p>
+            <p className='py-2 border-b mx-8>Send up to 10 GB</p>
         </div>
         <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-white'>Start Trial</button>
     </div>
@@ -35,9 +35,9 @@ const Cards = () => {
     <h2 className='text-2xl font-bold text-center py-8'>Group Account</h2>
     <p className='text-center text-4xl font-bold '>$299</p>
     <div className='text-center font-medium '>
-        <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-        <p className='py-2 border-b mx-8>1 Granted User</p>
-        <p className='py-2 border-b mx-8>Send up to 2 GB</p>
+        <p className='py-2 border-b mx-8 mt-8'>5 TB Storage</p>
+        <p className='py-2 border-b mx-8>10 Users Allowed</p>
+        <p className='py-2 border-b mx-8>Send up to 20 GB</p>
     </div>
     <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Start Trial</button>
 </div>
